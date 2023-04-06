1bbd367dbf38e484e1bb40d99e068f66764fd7e2
Guilherme Leoi <geeleonidas@pm.me>
2023-04-06 00:05:03 -0300

    Revert April Fools day

--- src/main/java/me/mateus/milkshake/core/milkshake/GeneratorThread.java
+++ src/main/java/me/mateus/milkshake/core/milkshake/GeneratorThread.java
@@ -253,7 +253,7 @@ public class GeneratorThread extends Thread {
     }
 
     private boolean isAprilFoolsToday() {
-        return Calendar.getInstance().get(Calendar.DAY_OF_MONTH) == 6 &&
+        return Calendar.getInstance().get(Calendar.DAY_OF_MONTH) == 1 &&
             Calendar.getInstance().get(Calendar.MONTH) == Calendar.APRIL;
     }
 }
