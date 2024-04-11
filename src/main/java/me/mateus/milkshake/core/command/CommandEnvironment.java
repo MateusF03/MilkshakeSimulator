1bcb80ac8a76b336cd1441db08417b311b63ac22
Guilherme Leoi <geeleonidas@pm.me>
2024-04-11 20:58:30 -0300

    Add `success` hook and `edit` procedure

--- src/main/java/me/mateus/milkshake/core/command/CommandEnvironment.java
+++ src/main/java/me/mateus/milkshake/core/command/CommandEnvironment.java
@@ -5,8 +5,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.function.Consumer;
 
 import javax.imageio.ImageIO;
+import javax.management.InvalidAttributeValueException;
+
+import org.jetbrains.annotations.Nullable;
 
 import kotlin.NotImplementedError;
 import net.dv8tion.jda.api.entities.Message;
@@ -14,6 +18,8 @@ import net.dv8tion.jda.api.entities.Message.Attachment;
 import net.dv8tion.jda.api.events.Event;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.interactions.InteractionHook;
+import net.dv8tion.jda.internal.utils.tuple.Pair;
 
 public final class CommandEnvironment {
     private Event event;
@@ -22,21 +28,25 @@ public final class CommandEnvironment {
         this.event = event;
     }
 
-    public void reply(String replyContent) {
+    public void reply(String replyContent, @Nullable Consumer<Pair<Message, InteractionHook>> success) {
         if (this.event instanceof MessageReceivedEvent) {
             MessageReceivedEvent realEvent =
                 (MessageReceivedEvent) this.event;
             realEvent.getChannel().sendMessage(replyContent)
-                .queue();
+                .queue(m -> success.accept(Pair.of(m, null)));
         } else if (this.event instanceof SlashCommandInteractionEvent) {
             SlashCommandInteractionEvent realEvent =
                 (SlashCommandInteractionEvent) this.event;
             realEvent.reply(replyContent)
                 .setEphemeral(true) // TODO: make this be parametric
-                .queue(); 
+                .queue(i -> success.accept(Pair.of(null, i))); 
         } else {
             throw new NotImplementedError(this.event + " is not being handled by `CommandEnvironment`");
         }
+    }
+
+    public void reply(String replyContent) {
+        reply(replyContent, null);
     }
 
     public BufferedImage getAttatchedImage() {
@@ -57,6 +67,16 @@ public final class CommandEnvironment {
         }
 
         return inputStreamToImage(stream);
+    }
+
+    public static void edit(Pair<Message, InteractionHook> reply, String newReplyContent) throws InvalidAttributeValueException {
+        if (reply.getRight() == null) { // MessageReceivedEvent
+            Message m = reply.getLeft();
+            m.editMessage(newReplyContent).queue();
+        } else { // SlashCommandInteractionEvent
+            InteractionHook i = reply.getRight();
+            i.editOriginal(newReplyContent).queue();
+        }
     }
 
     private BufferedImage inputStreamToImage(InputStream inputStream) {
