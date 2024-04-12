be0816a3af7a8cd48faa63d51282cb5419e0d983
Guilherme Leoi <geeleonidas@pm.me>
2024-04-12 15:29:16 -0300

    Add `onSlashCommandInteraction`

--- src/main/java/me/mateus/milkshake/core/command/CommandListener.java
+++ src/main/java/me/mateus/milkshake/core/command/CommandListener.java
@@ -1,5 +1,6 @@
 package me.mateus.milkshake.core.command;
 
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -9,6 +10,13 @@ public class CommandListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         if (event.getAuthor().isBot() || !event.isFromGuild())
+            return;
+        CommandManager.getInstance().runCommand(new CommandEnvironment(event));
+    }
+
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        if (event.getGuild() == null)
             return;
         CommandManager.getInstance().runCommand(new CommandEnvironment(event));
     }
