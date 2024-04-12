1150a36e68b37e0db60974494315459f5d2e9c31
Guilherme Leoi <geeleonidas@pm.me>
2024-04-12 14:48:54 -0300

    Adapt methods from `CreateCommands`

--- src/main/java/me/mateus/milkshake/commands/CreateCommands.java
+++ src/main/java/me/mateus/milkshake/commands/CreateCommands.java
@@ -2,6 +2,7 @@ package me.mateus.milkshake.commands;
 
 import com.google.gson.JsonObject;
 import me.mateus.milkshake.core.command.ArgumentType;
+import me.mateus.milkshake.core.command.CommandEnvironment;
 import me.mateus.milkshake.core.command.CommandManager;
 import me.mateus.milkshake.core.command.RegisteredCommand;
 import me.mateus.milkshake.core.command.interfaces.Argument;
@@ -15,7 +16,6 @@ import me.mateus.milkshake.core.milkshake.SourceRegion;
 import me.mateus.milkshake.core.milkshake.Template;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import javax.imageio.ImageIO;
 
@@ -35,10 +35,10 @@ public class CreateCommands {
             @Argument(name = "name", type = ArgumentType.STRING, obligatory = true),
             @Argument(name = "regions", type = ArgumentType.REGIONS, obligatory = true)
     })
-    public void create(MessageReceivedEvent event, ArgumentTranslator argumentTranslator) {
+    public void create(CommandEnvironment env, ArgumentTranslator argumentTranslator) {
         String name = argumentTranslator.getAsString("name");
         if (name.length() > 64) {
-            event.getChannel().sendMessage("**O limite de caracteres no nome é 64**").queue();
+            env.reply("**O limite de caracteres no nome é 64**");
             return;
         }
         List<SourceRegion> regions = argumentTranslator.getAsRegions("regions");
@@ -58,25 +58,19 @@ public class CreateCommands {
                 }
             }
             if (!isAvaiable) {
-                event.getChannel().sendMessage("**Fonte **`" + regionFont + "`** não está disponível**").queue();
+                env.reply("**Fonte **`" + regionFont + "`** não está disponível**");
                 return;
             }
         }
 
         if (t != null) {
-            event.getChannel().sendMessage("**Já existe um template com este nome**").queue();
+            env.reply("**Já existe um template com este nome**");
             return;
         }
 
-        InputStream inputStream = getInputStreamFromMessage(event.getMessage());
-
-        if (inputStream == null) {
-            event.getChannel().sendMessage("**Não foi possível reconhecer a imagem**").queue();
-            return;
-        }
-        BufferedImage image = inputStreamToImage(inputStream);
+        BufferedImage image = env.getAttatchedImage();
         if (image == null) {
-            event.getChannel().sendMessage("**Não foi possível reconhecer a imagem**").queue();
+            env.reply("**Não foi possível reconhecer a imagem**");
             return;
         }
         String fileName = UUID.randomUUID() + ".png";
@@ -84,7 +78,7 @@ public class CreateCommands {
         try {
             ImageIO.write(image, "png", file);
         } catch (IOException e) {
-            event.getChannel().sendMessage("**Erro ao salvar imagem**").queue();
+            env.reply("**Erro ao salvar imagem**");
             e.printStackTrace();
             return;
         }
@@ -97,13 +91,8 @@ public class CreateCommands {
             template.setOriginalFilePath(templateFile.getPath());
             template.setWidth(image.getWidth());
             template.setHeight(image.getHeight());
-            event.getChannel().sendMessage("**Template criado!**").queue();
+            env.reply("**Template criado!**");
             manager.addTemplate(template);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            inputStream.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -112,17 +101,17 @@ public class CreateCommands {
     @Command(name = "add", description = "Adiciona um texto", args = {
             @Argument(name = "text", type = ArgumentType.STRING, obligatory = true)
     })
-    public void add(MessageReceivedEvent event, ArgumentTranslator translator) {
+    public void add(CommandEnvironment env, ArgumentTranslator translator) {
 
         MilkshakeManager manager = MilkshakeManager.getInstance();
         try {
             String text = translator.getAsString("text");
             if (text.length() > 64) {
-                event.getChannel().sendMessage("**O limite de caracteres é 64**").queue();
+                env.reply("**O limite de caracteres é 64**");
                 return;
             }
             manager.addText(text);
-            event.getChannel().sendMessage("**O texto:** `" + text + "` ** foi adicionado às sources**").queue();
+            env.reply("**O texto:** `" + text + "` ** foi adicionado às sources**");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -131,41 +120,36 @@ public class CreateCommands {
     @Command(name = "eliminar", description = "Deleta um template", vipOnly = true, args = {
             @Argument(name = "template", type = ArgumentType.STRING, obligatory = true)
     })
-    public void delete(MessageReceivedEvent event, ArgumentTranslator translator) {
+    public void delete(CommandEnvironment env, ArgumentTranslator translator) {
         String name = translator.getAsString("template");
         MilkshakeManager manager = MilkshakeManager.getInstance();
         Template t = manager.getTemplateByName(name);
 
         if (t == null) {
-            event.getChannel().sendMessage("**Não existe este template**").queue();
+            env.reply("**Não existe este template**");
             return;
         }
         if (!t.delete()) {
-            event.getChannel().sendMessage("**Erro ao deletar template!**").queue();
+            env.reply("**Erro ao deletar template!**");
         } else {
             manager.getTemplates().remove(t);
-            event.getChannel().sendMessage("**Template deletado!**").queue();
+            env.reply("**Template deletado!**");
         }
     }
 
     @Command(name = "source", description = "Cria uma source", receivesImage = true, args = {
             @Argument(name = "name", type = ArgumentType.STRING, obligatory = true)
     })
-    public void source(MessageReceivedEvent event, ArgumentTranslator translator) {
+    public void source(CommandEnvironment env, ArgumentTranslator translator) {
         String name = translator.getAsString("name");
         if (name.length() > 64) {
-            event.getChannel().sendMessage("**O limite de caracteres no nome é 64**").queue();
+            env.reply("**O limite de caracteres no nome é 64**");
             return;
         }
-        InputStream inputStream = getInputStreamFromMessage(event.getMessage());
 
-        if (inputStream == null) {
-            event.getChannel().sendMessage("**Não foi possível reconhecer a imagem**").queue();
-            return;
-        }
-        BufferedImage image = inputStreamToImage(inputStream);
+        BufferedImage image = env.getAttatchedImage();
         if (image == null) {
-            event.getChannel().sendMessage("**Não foi possível reconhecer a imagem**").queue();
+            env.reply("**Não foi possível reconhecer a imagem**");
             return;
         }
 
@@ -178,7 +162,7 @@ public class CreateCommands {
             Files.writeString(file.toPath(), json);
             MilkshakeManager.getInstance().addSource(source);
 
-            event.getChannel().sendMessage("**Source adicionada!**").queue();
+            env.reply("**Source adicionada!**");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -188,18 +172,18 @@ public class CreateCommands {
             @Argument(name = "oldName", type = ArgumentType.STRING, obligatory = true),
             @Argument(name = "newName", type = ArgumentType.STRING, obligatory = true)
     })
-    public void rename(MessageReceivedEvent event, ArgumentTranslator translator) {
+    public void rename(CommandEnvironment env, ArgumentTranslator translator) {
         String oldName = translator.getAsString("oldName");
         String newName = translator.getAsString("newName");
         List<Source> sources = MilkshakeManager.getInstance().getSourcesByName(oldName);
 
         if (sources.isEmpty()) {
-            event.getChannel().sendMessage("**Não existe nenhuma source com este nome**").queue();
+            env.reply("**Não existe nenhuma source com este nome**");
         } else if (sources.size() == 1) {
             Source source = sources.get(0);
             try {
                 source.rename(newName);
-                event.getChannel().sendMessage("**Source renomeada para:** `" + newName + "`").queue();
+                env.reply("**Source renomeada para:** `" + newName + "`");
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -217,13 +201,19 @@ public class CreateCommands {
             description.append("`\n**Digite:** `Nº da source | novo nome` **para trocar de nome e** `Nº da source` **apenas para ver a imagem da source**");
             embedBuilder.setDescription(description.toString());
             embedBuilder.setFooter("Digite &&cancel para cancelar");
-            event.getChannel().sendMessageEmbeds(embedBuilder.build()).queue();
-            event.getJDA().addEventListener(new RenameSourceListener(event.getAuthor().getIdLong(),event.getChannel().getIdLong() , sources));
+            env.embed(embedBuilder.build());
+            env.getJDAInstance().addEventListener(
+                new RenameSourceListener(
+                    env.getCommandCaller().getIdLong(),
+                    env.getCallingChannel().getIdLong(),
+                    sources
+                )
+            );
         }
     }
 
     @Command(name = "guide", description = "Envia um guia de como criar template")
-    public void guide(MessageReceivedEvent event, ArgumentTranslator translator) {
+    public void guide(CommandEnvironment env, ArgumentTranslator translator) {
         EmbedBuilder embedBuilder = new EmbedBuilder();
         embedBuilder.setTitle("Guia de como criar templates:");
         embedBuilder.setColor(0xb28dff);
@@ -247,12 +237,12 @@ public class CreateCommands {
                 **Lista de cores:** <https://imagemagick.org/script/color.php#color_names>
                 **Lista de orientações:** <https://www.imagemagick.org/script/command-line-options.php#gravity>
                 **Lista de fontes:** use `magick identify -list font` para pegar todas fontes""";
-        event.getChannel().sendMessageEmbeds(embedBuilder.build()).queue();
-        event.getChannel().sendMessage(links).queue();
+        env.embed(embedBuilder.build());
+        env.reply(links); // FIXME: this won't work on current implementation
     }
 
     @Command(name = "help", description = "O comando para listar todos os outros comandos do bot")
-    public void help(MessageReceivedEvent event, ArgumentTranslator translator)  {
+    public void help(CommandEnvironment env, ArgumentTranslator translator)  {
         EmbedBuilder embedBuilder = new EmbedBuilder();
         embedBuilder.setTitle("Comandos:");
         embedBuilder.setColor(0xb28dff);
@@ -265,26 +255,26 @@ public class CreateCommands {
         }
         description.append('`');
         embedBuilder.setDescription(description.toString());
-        event.getChannel().sendMessageEmbeds(embedBuilder.build()).queue();
+        env.embed(embedBuilder.build());
     }
 
     @Command(name = "eliminarSource", description = "Elimina uma source",vipOnly = true, args = {
             @Argument(name = "sourceName", type = ArgumentType.STRING, obligatory = true)
     })
-    public void removeSource(MessageReceivedEvent event, ArgumentTranslator translator) {
+    public void removeSource(CommandEnvironment env, ArgumentTranslator translator) {
         String sourceName = translator.getAsString("sourceName");
         List<Source> sources = MilkshakeManager.getInstance().getSourcesByName(sourceName);
 
         if (sources.isEmpty()) {
-            event.getChannel().sendMessage("**Não existe nenhuma source com este nome**").queue();
+            env.reply("**Não existe nenhuma source com este nome**");
         } else if (sources.size() == 1) {
             Source source = sources.get(0);
             if (source.errorWhileDeleting()) {
-                event.getChannel().sendMessage("**Erro ao deletar source**").queue();
+                env.reply("**Erro ao deletar source**");
                 return;
             }
             MilkshakeManager.getInstance().getSources().remove(source);
-            event.getChannel().sendMessage("**Source deletada!**").queue();
+            env.reply("**Source deletada!**");
 
         } else {
             EmbedBuilder embedBuilder = new EmbedBuilder();
@@ -300,15 +290,21 @@ public class CreateCommands {
             description.append("`\n**Digite:** `Nº da source` **para deleter e** `&&ver Nº da source` **apenas para ver a imagem da source**");
             embedBuilder.setDescription(description.toString());
             embedBuilder.setFooter("Digite &&cancel para cancelar");
-            event.getChannel().sendMessageEmbeds(embedBuilder.build()).queue();
-            event.getJDA().addEventListener(new DeleteSourceListener(event.getAuthor().getIdLong(),event.getChannel().getIdLong() , sources));
+            env.embed(embedBuilder.build());
+            env.getJDAInstance().addEventListener(
+                new DeleteSourceListener(
+                    env.getCommandCaller().getIdLong(),
+                    env.getCallingChannel().getIdLong(),
+                    sources
+                )
+            );
         }
     }
 
     @Command(name = "fontes", description = "Lista as fontes instaladas no servidor", args = {
         @Argument(name = "page", type = ArgumentType.INTEGER, obligatory = false)
     })
-    public void listInstalledFonts(MessageReceivedEvent event, ArgumentTranslator translator) {
+    public void listInstalledFonts(CommandEnvironment env, ArgumentTranslator translator) {
         EmbedBuilder embedBuilder = new EmbedBuilder();
         embedBuilder.setTitle("Fontes disponíveis:");
         embedBuilder.setColor(0xb28dff);
@@ -326,7 +322,7 @@ public class CreateCommands {
         description.append("`");
         embedBuilder.setDescription(description.toString());
         embedBuilder.setFooter("Página " + page + " de " + lastPage);
-        event.getChannel().sendMessageEmbeds(embedBuilder.build()).queue();
+        env.embed(embedBuilder.build());
     }
 
     private String[] getInstalledFonts() {
